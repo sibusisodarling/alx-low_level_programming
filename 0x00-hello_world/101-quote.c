ce23d8623b0304901466c5a330a47d2c1a277966
sibusisodarling <sibusisotradestone@gmail.com>
2023-02-16 23:53:03 +0000

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of a program
+ *
+ * Return: -Always returns a 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful" 
+			\"- Dora Korpar, 2015-10-19", 59);
+	
+	return (1);
+}
