e76f68de29d2f144dbc095b041a24de29b0dcb3e
sibusisodarling <sibusisotradestone@gmail.com>
2023-02-18 12:12:02 +0000

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point of the pragram
+ * main - Entry point
  *
  * return: -Always returns 0
  */
 int main(void)
 {
-	int i;
-	int j;
-
-	for (j = '0'; j <= '9'; j++)
-	{
-		putchar(j);
-	}
-    	for (i = 'a'; i <= 'f'; i++)
-    	{
-        	putchar(i);
-    	}
-    	putchar('\n');
-    	return (0);
+    int i;
+    int j;
+    
+    for (j = '0'; j <= '9'; j++)
+    {
+	putchar(j);
+    }	
+    for (i = 'a'; i <= 'f'; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
 }
