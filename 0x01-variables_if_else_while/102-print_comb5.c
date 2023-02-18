ee618e26869a820c809a8aa4c58f240101bb30e0
sibusisodarling <sibusisotradestone@gmail.com>
2023-02-18 14:55:17 +0000

    a program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = j + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 98 && j == 99)
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
