eef82e831f9be30d2e61f595a3f37bace653fbe8
sibusisodarling <sibusisotradestone@gmail.com>
2023-02-22 02:03:22 +0000

    a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,9 @@
  */
 int _isalpha(int c)
 {
-	   if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-	   {
-        	return (1);
-    	   }
-    	   return (0);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
 }
