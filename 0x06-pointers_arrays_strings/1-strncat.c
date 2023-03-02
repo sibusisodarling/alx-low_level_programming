6823c0f1cad9d10b8596f5ff8410a33dce08b642
sibusisodarling <sibusisotradestone@gmail.com>
2023-03-02 21:10:33 +0000

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminating
- *          null byte, to the end of the string pointed to by @dest.
+ * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
@@ -12,8 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+	for (i = 0; dest[i] != '\0'; i++);
 
 	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
 	{
