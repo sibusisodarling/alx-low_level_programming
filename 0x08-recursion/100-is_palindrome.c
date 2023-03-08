7166e14f652605d730df92a61c83320aba123ff8
sibusisodarling <sibusisotradestone@gmail.com>
2023-03-08 23:00:40 +0000

    a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,7 +27,7 @@ int find_strlen(char *s)
  * check_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
  * @len: The length of s.
- * @index: The index of the string to be checked.
+ * @i: The index of the string to be checked.
  *
  * Return: If the string is a palindrome - 1.
  *         If the string is not a palindrome - 0.
