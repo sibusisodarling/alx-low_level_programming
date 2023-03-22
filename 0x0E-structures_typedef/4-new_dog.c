c7656b51b141cbf4562799a9d8eb149ec358c6a0
sibusisodarling <sibusisotradestone@gmail.com>
2023-03-22 02:15:44 +0000

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,26 +9,25 @@
  * @owner: char string owner
  * Return: pointer to new dog
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
-    if (new_dog == NULL) {
-        return NULL;
-    }
-    new_dog->name = (char *)malloc(strlen(name) + 1);
-    new_dog->owner = (char *)malloc(strlen(owner) + 1);
-    
-    if (new_dog->name == NULL || new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog->owner);
-        free(new_dog);
-        return (NULL);
-    }
-    strcpy(new_dog->name, name);
-    new_dog->age = age;
-    strcpy(new_dog->owner, owner);
-    
-    return (new_dog);
+	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = (char *)malloc(strlen(name) + 1);
+	new_dog->owner = (char *)malloc(strlen(owner) + 1);
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	new_dog->age = age;
+	strcpy(new_dog->owner, owner);
+	return (new_dog);
 }
