ef5ac6c743e275170b3663329426fe39cdbe804d
sibusisodarling <sibusisotradestone@gmail.com>
2023-03-22 02:24:50 +0000

    a function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dog - frees a dog structure
+ * @d: pointer to struct to free
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+	}
+	free(d);
+	}
+}
